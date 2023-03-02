133a148d0917510ea7a044f8f98edcac3ae2424a
tluu9798 <trungluu9798@gmail.com>
2023-03-01 22:38:28 -0500

    Initial commit

---
+++ StaticList/StaticListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticListApp.swift
+//  StaticList
+//
+//  Created by Trung Luu on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct StaticListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
